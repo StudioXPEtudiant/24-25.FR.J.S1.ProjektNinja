7f3a345cb1f73773a1e3f14213846f19a0cca363
Andresgas <andresgascon757@gmail.com>
2025-03-06 19:34:17 +0100

    Debut de map dans main et amelioration mouvements perso principal

--- Assets/PAUL/Script/MovePlayer.cs
+++ Assets/PAUL/Script/MovePlayer.cs
@@ -12,167 +12,94 @@ namespace PAUL.Script
         [SerializeField] private Transform pos;
         [SerializeField] private Rigidbody playerRb;
         
-        // Variables and Stats
-        [SerializeField] private float speed = 15f;
-        
-        Vector3 temp = Vector3.zero;
-        private string Axis = "z";
-        
-        
-        // Dash variables
-        [SerializeField] private float dashTimer = 2f;
-        [SerializeField] private float dashPower = 5f;
-        [SerializeField] private float dashCooldown = 15f;
-        
-        private bool CanDash = true;
-        private float DHTR;
-        private float DHCD;
+        // Variables et stats
+        [SerializeField] private float speed = 2f; // Vitesse normale ajustée
+        [SerializeField] private float dashPower = 10f; // Puissance du dash
+        [SerializeField] private float dashDuration = 0.2f; // Durée du dash
+        [SerializeField] private float dashCooldown = 2f; // Cooldown du dash
+        private bool isDashing = false;
+        private bool canDash = true;
+        private float dashTimeRemaining;
+        private float dashCooldownRemaining;
 
-        // Speed Boost variables
-        [SerializeField] private float SpeedBoostTimer = 4f;
-        [SerializeField] private float SpeedBoostCooldown = 15f;
-        [SerializeField] private float SpeedBoostValue = 10f;
-
-        private bool CanSpeedBoost = true;
-        private float SBTR;
-        private float SBCD;
+        // Variables internes
+        private Vector3 movementDirection = Vector3.zero;
         
-
-        
-        
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
-
-        // Update is called once per frame
         void Update()
         {
-            temp = Vector3.zero;
-            Axis = "z";
-            
-            // Player movements
-            if (Input.GetKey(KeyCode.W))
-            {
-                temp.x=1;
-                Axis = "z";
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                temp.x=-1;
-                Axis = "s";
-            }
-        
-            if (Input.GetKey(KeyCode.A))
-            {
-                temp.z=1;
-                Axis = "q";
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                temp.z=-1;
-                Axis = "d";
-            }
+            movementDirection = Vector3.zero;
 
-            pos.position += temp * (Time.deltaTime * speed);
-            
-            //Dash Timer
-            if (DHTR > 0)
-            {
-                DHTR -= Time.deltaTime;
-            }
+            // Déplacement avec ZQSD
+            if (Input.GetKey(KeyCode.W)) movementDirection.x = 1;
+            else if (Input.GetKey(KeyCode.S)) movementDirection.x = -1;
 
-            if (DHTR < 0)
-            {
-                DHTR = 0;
-                
-            }
-            
-            //Dash Cooldown 
-            if (DHCD > 0)
-            {
-                DHCD -= Time.deltaTime;
-            }
+            if (Input.GetKey(KeyCode.A)) movementDirection.z = 1;
+            else if (Input.GetKey(KeyCode.D)) movementDirection.z = -1;
 
-            if (DHCD < 0)
-            {
-                DHCD = 0;
-                CanDash = true;
-            }
-            
             // Dash
-            if (Input.GetKeyDown(KeyCode.LeftShift) && (CanDash == true) && (DHCD == 0))
+            if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
             {
-                DHTR = dashTimer;
-                CanDash = false;
-                playerRb.useGravity = false;
-                if (Axis == "z")
-                {
-                    playerRb.velocity = new Vector3(pos.localScale.x * dashPower, 0f, 0f);
-                }
-                else if (Axis == "s")
-                {
-                    playerRb.velocity = new Vector3(pos.localScale.x * -dashPower, 0f,0f );
-                }
-                else if (Axis == "d")
-                {
-                    playerRb.velocity = new Vector3(0f, 0f, pos.localScale.z * -dashPower);
-                }
-                else if (Axis == "q")
-                {
-                    playerRb.velocity = new Vector3(0f, 0f, pos.localScale.z * dashPower);
-                }
-            }
-            else if ((DHTR == 0) && (CanDash == false))
-            {
-                DHCD = dashCooldown;
-                playerRb.useGravity = true;
-                playerRb.velocity = Vector3.zero;
-                CanDash = true;
-            }
-            
-            
-            
-            //SpeedBoost Timer
-            if (SBTR > 0)
-            {
-                SBTR -= Time.deltaTime;
+                StartDash();
             }
 
-            if (SBTR < 0)
+            // Gestion du cooldown du dash
+            if (dashCooldownRemaining > 0)
             {
-                SBTR = 0;
-                
+                dashCooldownRemaining -= Time.deltaTime;
+                if (dashCooldownRemaining <= 0) canDash = true;
             }
+        }
 
-            
-            //SpeedBoost Cooldown 
-            if (SBCD > 0)
+        void FixedUpdate()
+        {
+            if (isDashing)
             {
-                SBCD -= Time.deltaTime;
+                dashTimeRemaining -= Time.fixedDeltaTime;
+                if (dashTimeRemaining <= 0)
+                {
+                    StopDash();
+                }
             }
+            else
+            {
+                ApplyMovement();
+            }
+        }
 
-            if (SBCD < 0)
+        private void ApplyMovement()
+        {
+            if (movementDirection != Vector3.zero)
             {
-                SBCD = 0;
-                
+                Vector3 force = movementDirection.normalized * speed;
+                playerRb.AddForce(force, ForceMode.VelocityChange);
+
+                // Limiter la vitesse pour éviter l'accélération infinie
+                Vector3 horizontalVelocity = new Vector3(playerRb.velocity.x, 0f, playerRb.velocity.z);
+                if (horizontalVelocity.magnitude > speed)
+                {
+                    horizontalVelocity = horizontalVelocity.normalized * speed;
+                    playerRb.velocity = new Vector3(horizontalVelocity.x, playerRb.velocity.y, horizontalVelocity.z);
+                }
             }
-            
-            // SpeedBoost
-            if (Input.GetKeyDown(KeyCode.G) && (CanSpeedBoost == true) && (SBCD == 0))
-            {
-                SBTR = SpeedBoostTimer;
-                CanSpeedBoost = false;
-                speed += SpeedBoostValue;
-            }
-            else if ((SBTR == 0) && (CanSpeedBoost == false))
-            {
-                    SBCD = SpeedBoostCooldown;
-                    speed -= SpeedBoostValue;
-                    CanSpeedBoost = true;
-            }
-            
+        }
+
+        private void StartDash()
+        {
+            isDashing = true;
+            canDash = false;
+            dashTimeRemaining = dashDuration;
+            dashCooldownRemaining = dashCooldown;
+
+            // Appliquer la vélocité du dash dans la direction actuelle
+            Vector3 dashVelocity = movementDirection.normalized * dashPower;
+            playerRb.velocity = new Vector3(dashVelocity.x, playerRb.velocity.y, dashVelocity.z);
+            playerRb.useGravity = false; // Désactive la gravité pour un dash plus fluide
+        }
+
+        private void StopDash()
+        {
+            isDashing = false;
+            playerRb.useGravity = true; // Réactive la gravité après le dash
         }
     }
 }
--- Assets/PAUL/Script/TrackingFunction.cs
+++ Assets/PAUL/Script/TrackingFunction.cs
@@ -1,37 +1,92 @@
 using System;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;  // Ajouté pour l'utilisation d'IEnumerator
 
 namespace Script
 {
+    [RequireComponent(typeof(NavMeshAgent))]
     public class TrackingFunction : MonoBehaviour
     {
-        [SerializeField] private float radius = 5;
-        [SerializeField] private Transform self;
-        [SerializeField] private Transform target;
-        public NavMeshAgent agent;
-        private double distance = 1d;
+        [SerializeField] private float radius = 5f;  // Rayon d'activation pour suivre
+        [SerializeField] private Transform target;   // Joueur à suivre
+        private NavMeshAgent agent;
+        private Collider targetCollider; // Collider du joueur
 
-        // Start is called before the first frame update
-        void Start()
+        void Awake()
         {
-    
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            distance = Math.Sqrt((Math.Pow(self.position.x - target.position.x, 2)) +
-                                 (Math.Pow(self.position.y - target.position.y, 2)) +
-                                 (Math.Pow(self.position.z - target.position.z, 2)));
-                         
-
-            if (radius >= distance)
+            // On s'assure qu'un NavMeshAgent est attaché à cet objet
+            agent = GetComponent<NavMeshAgent>();
+            if (agent == null)
             {
-                agent.SetDestination(target.position);
+                Debug.LogError("Le NavMeshAgent n'est pas attaché à cet objet !", this);
+                return; // Arrête l'exécution si l'agent n'est pas trouvé
             }
 
+            // Si un Rigidbody est attaché, on le met en Kinematic pour éviter les conflits de physique
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = true;  // Rend l'objet non affecté par la physique
+            }
+
+            // Récupérer le Collider du joueur (target)
+            if (target != null)
+            {
+                targetCollider = target.GetComponent<Collider>();
+            }
         }
 
+        void Start()
+        {
+            // Si un Collider du joueur est trouvé, on ignore les collisions entre l'objet traqueur et le joueur
+            if (targetCollider != null)
+            {
+                Collider thisCollider = GetComponent<Collider>();
+                if (thisCollider != null)
+                {
+                    Physics.IgnoreCollision(thisCollider, targetCollider, true); // Ignore la collision
+                }
+            }
+        }
+
+        void Update()
+        {
+            // Vérifier si target est assigné
+            if (target == null)
+            {
+                Debug.LogWarning("Target (le joueur) n'est pas assigné dans l'inspecteur !");
+                return;
+            }
+
+            // Vérification de la distance entre l'objet et le joueur
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            if (distance <= radius)
+            {
+                // Si l'agent quitte le NavMesh, on le replace à la position du joueur
+                if (!agent.isOnNavMesh)
+                {
+                    agent.Warp(target.position);  // Ramène l'agent sur le NavMesh
+                }
+
+                // Donne la destination à l'agent
+                agent.SetDestination(target.position);
+            }
+        }
+
+        // Gérer les collisions pour éviter des comportements anormaux
+        void OnCollisionEnter(Collision collision)
+        {
+            StartCoroutine(StopTrackingTemporarily());
+        }
+
+        // Arrêter temporairement le suivi après une collision pour éviter le bug
+        IEnumerator StopTrackingTemporarily()
+        {
+            agent.isStopped = true;
+            yield return new WaitForSeconds(0.5f); // Pause de 0.5 seconde
+            agent.isStopped = false;
+        }
     }
 }
